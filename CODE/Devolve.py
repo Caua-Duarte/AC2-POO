ebd93d7e4a9a68e17996a87a5502110a3306d790
Cauã Duarte Sampaio <77754362+Caua-Duarte@users.noreply.github.com>
2021-11-25 16:42:53 -0300

    Delete Devolve.py

--- CODE/Devolve.py
+++
@@ -1,8 +0,0 @@
-from Veiculo import *
-
-def CKlistEntrega(self):
-    if self.ck_list_entrega == True:
-        print("Carro esta liberado")
-    else:
-        print("Carro não esta conforme")
-        
